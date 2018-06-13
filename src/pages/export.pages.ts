59e3e88f2ff842b895d2cec6bd33a5f911cfdd8e
Yexur <shimojax@gmail.com>
2018-06-13 12:42:09 -0500

    Added the tab pages
    Added the tab pages

---
+++ src/pages/export.pages.ts
@@ -0,0 +1,7 @@
+export * from './home/home';
+export * from './daily-goals/daily-goals';
+export * from './weekly-goals/weekly-goals';
+export * from './monthly-goals/monthly-goals';
+export * from './other-goals/other-goals';
+
+//TODO: we get rid of this page.  it might not be needed using lazy loading
---
+++ src/pages/other-goals/other-goals.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { OtherGoalsPage } from './other-goals';
+
+@NgModule({
+  declarations: [
+    OtherGoalsPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(OtherGoalsPage),
+  ],
+})
+export class OtherGoalsPageModule {}
